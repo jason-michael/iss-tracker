36820e7e868084dcd03fd835570310b5a6027fac
Jason Michael <michael.jason@outlook.com>
2018-09-27 13:19:13 -0500

    added demo functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,31 @@
-// #region Global vars
 const SEARCHBAR = $('#search-input');
 const SEARCHBUTTON = $('#submit-button');
 const GOOGLE_API_KEY = 'AIzaSyD-kc7RMsdE13o6eAQEWeQECWzP8AJSr_A';
-
 let ISSLocation;
 let ISSCrew;
-let streetAddress;
-let geocodeLocation;
-// #endregion Global vars
 
-// #region Initialization
-getISSLocation();
-getISSCrew();
-// #endregion Initialization
+/* INITIALIZATION */
+// Init the address autocompletion
+google.maps.event.addDomListener(window, 'load', initAutocomplete);
 
-// #region Events
-$(SEARCHBUTTON).on('click', function(e) {
-    
+// TODO: Demo item, remove.
+runDemo();
+
+/* EVENTS */
+$(SEARCHBUTTON).on('click', function (e) {
     e.preventDefault();
-
-    streetAddress = SEARCHBAR.val().trim();
-
+    let streetAddress = SEARCHBAR.val().trim();
     getGeocode(streetAddress);
-
-    // Clear the search input after search.
-    SEARCHBAR.val('');
-
 });
-// #endregion Events
 
-// #region Functions
+/* FUNCTIONS */
+/**
+ * Attaches the Google Places Autocomplete API to the searchbar.
+ */
+function initAutocomplete() {
+    let searchbar = document.getElementById('search-input');
+    new google.maps.places.Autocomplete(searchbar);
+}
 
 /**
  * Sets geocodeLocation for a given address.
@@ -45,14 +41,32 @@ function getGeocode(address) {
     $.ajax({
         method: 'GET',
         url: queryURL
-    }).done(function(response){
-        geocodeLocation = {
+    }).done(function (response) {
+        let geocodeLocation = {
             lat: response.results[0].geometry.location.lat,
             lng: response.results[0].geometry.location.lng
         };
 
-        // Todo: remove...
-        console.log(geocodeLocation)
+        // TODO: Demo item, remove.
+        console.log('Geocode for: ' + address, geocodeLocation);
+    });
+}
+
+function getReversedGeocode(lat, lng) {
+
+    let queryURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`;
+
+    $.ajax({
+        method: 'GET',
+        url: queryURL
+    }).done(function (response) {
+
+        // TODO: Demo, remove.
+        if (response.results.length === 0) {
+            console.log('No address found for:', lat, lng, 'See current location on map.');
+        } else {
+            console.log('The ISS is over:', response.results[0].formatted_address);
+        }
     });
 }
 
@@ -70,11 +84,15 @@ function getISSLocation() {
         }
     }).done(function (response) {
         ISSLocation = {
-            lat: response.iss_position.latitude,
-            lng: response.iss_position.longitude
+            lat: parseFloat(response.iss_position.latitude),
+            lng: parseFloat(response.iss_position.longitude)
         };
-    });
 
+        getReversedGeocode(ISSLocation.lat, ISSLocation.lng);
+
+        // TODO: Demo item, remove.
+        console.log('Current ISS location: ', ISSLocation);
+    });
 }
 
 /**
@@ -91,6 +109,62 @@ function getISSCrew() {
         }
     }).done(function (response) {
         ISSCrew = response.people;
+
+        // TODO: Demo item, remove.
+        console.log('ISS crew members: ', ISSCrew);
     });
 }
-// #endregion Functions
+
+// TODO: Demo item, remove.
+function runDemo() {
+    getISSCrew();
+    getISSLocation();
+}
+
+// !! EXPIRIMENTAL !!
+/**
+ * Grabs the entire infobox from a wikipedia page.
+ * @param {string} title The title of the Wikipedia page to scrape.
+ */
+function scrapeWikiInfoBox(title) {
+
+    let _title = title.replace(/ /g, '_');
+
+    $.ajax({
+        type: "GET",
+        url: `http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=${_title}&callback=?`,
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data) {
+
+            let markup = data.parse.text["*"];
+            let blurb = $('<div></div>').html(markup);
+            // Replace links with text, they won't work anyway
+            blurb.find('a').each(function () {
+                $(this).replaceWith($(this).html());
+            });
+
+            let nickname;
+            nickname = blurb.find('span.nickname').text();
+            console.log('Nickname: ' + nickname);
+
+            // Get crew member img src
+            let imgUrls = [];
+            blurb.find('img').each(function () {
+                imgUrls.push($(this).attr('src'));
+            });
+            imgSrc = imgUrls[0];
+            console.log('Img url: ' + imgSrc);
+
+            // Get crew member age
+            let age = blurb.find('span.ForceAgeToShow').text();
+            console.log('Age: ' + age.match(/\d+/g).map(Number));
+
+            $('#markup').html($(blurb).find('.infobox'));
+        },
+        error: function (errorMessage) {
+            console.log('Error: ' + errorMessage);
+        }
+    });
+}
